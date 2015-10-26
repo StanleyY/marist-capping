e233d18c1d635fb5d37238c9a916ad7231421ec2
Stanley Yang <stanley.yang.sy@gmail.com>
2015-10-26 02:27:02 -0400

    recreated database

--- capping/views.py
+++ capping/views.py
@@ -1,6 +1,14 @@
+import json
+
 from django.http import HttpResponse
 from django.shortcuts import render
 
+
 # Create your views here.
 def homeView(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
+  return HttpResponse("Hello, world. You're at the polls index.")
+
+
+def getClass(request):
+  data = json.dumps({'key': "stuff"})
+  return HttpResponse(data)
