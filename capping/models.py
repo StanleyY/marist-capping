bc2ddd3044e4a35b24c91c2b9b5798566a804371
Stanley Yang <stanley.yang.sy@gmail.com>
2015-11-02 01:09:12 -0500

    improved mapping model

--- capping/models.py
+++ capping/models.py
@@ -11,3 +11,11 @@ class InternalCourse(models.Model):
   subject = models.CharField(max_length=4)
   number = models.CharField(max_length=4)
   title = models.CharField(max_length=30)
+
+
+class Mapping(models.Model):
+  external = models.ForeignKey('ExternalCourse')
+  internal = models.ForeignKey('InternalCourse')
+
+  class Meta:
+    unique_together = (('external', 'internal'),)
