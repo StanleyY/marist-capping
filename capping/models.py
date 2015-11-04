ef8ce37b98b62f9e7a06a6cba83cfd0d20144b93
OneLuminare <Nathan.Fahrner1@marist.edu>
2015-11-04 14:39:48 -0500

    Created models for course requirements. Wish it supported compound primary keys, but as it does not, I mostly used autoid and created unique constraints. I edited two of models existing to have unique constraints(internal , external). I listed all me edits with notes. I will work on views soon.

--- capping/models.py
+++ capping/models.py
@@ -2,16 +2,25 @@ from django.db import models
 
 # Create your models here.
 class ExternalCourse(models.Model):
+  # OneLuminare Edit
+  college = models.CharField(max_length=75)
+  #--
   subject = models.CharField(max_length=4)
   number = models.CharField(max_length=4)
   title = models.CharField(max_length=30)
-
+  # Edit
+  class Meta:
+	unique_together = (('college','subject','number'),)
+  #--
 
 class InternalCourse(models.Model):
   subject = models.CharField(max_length=4)
   number = models.CharField(max_length=4)
   title = models.CharField(max_length=30)
-
+  # Edit
+  class Meta:
+	unique_together = (('subject','number'),)
+  #--
 
 class Mapping(models.Model):
   external = models.ForeignKey('ExternalCourse')
@@ -20,8 +29,42 @@ class Mapping(models.Model):
   class Meta:
     unique_together = (('external', 'internal'),)
 
+# Major Requirements Models
 
-# TODO: Delete and redo this
-class MajorReq(models.Model):
-  major = models.CharField(max_length=120)
-  course = models.CharField(max_length=30)
+# OneLuminare edit
+class Majors(models.Model):
+	major = models.CharField(max_length=100)
+
+class MajorRequirements(models.Model):
+	major = models.ForeignKey('Majors')
+	internal = modles.ForeignKey('InternalCourse')
+	
+	class Meta:
+		unique_together(('major','internal'),)
+		
+class OfListItems(models.Model):
+    oid = models.IntegerField(primary_key=true)
+	major = models.ForeignKey('Majors')
+	number_selected = models.IntegerField()
+	
+class OfListData(models.Model):
+	oid = models.ForeignKey('OfListItems')
+	internal = models.ForeignKey('Internal')
+	
+	class Meta:
+		unique_together(('oid','internal'),)
+	
+class OfSetItems(models.Model):
+    osid = models.IntegerField(primary_key=true)
+	major = models.ForeignKey('Majors')
+	number_selected = models.IntegerField()
+	set_size = models.IntegerField()
+	
+class OfSetData(models.Model):
+	osid = models.ForeignKey('OfSetItems')
+	setid = models.IntegerField()
+	internal = models.ForeignKey('Internal')
+	
+	class Meta:
+		unique_together(('osid','setid','internal'),)
+#--
